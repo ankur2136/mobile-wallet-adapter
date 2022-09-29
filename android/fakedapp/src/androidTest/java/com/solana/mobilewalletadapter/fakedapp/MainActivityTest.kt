c13f73937448deb77ad05b206a93f417e8a3c8f0
Ankur Jain <ankur.jain@solana.com>
2022-09-29 14:43:52 -0700

    Add test runner and annotation

--- android/fakedapp/src/androidTest/java/com/solana/mobilewalletadapter/fakedapp/MainActivityTest.kt
+++ android/fakedapp/src/androidTest/java/com/solana/mobilewalletadapter/fakedapp/MainActivityTest.kt
@@ -9,11 +9,13 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class MainActivityTest {
     @get:Rule var activityScenarioRule = activityScenarioRule<MainActivity>()
 
